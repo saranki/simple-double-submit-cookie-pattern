1155f6a9c23ad415665c51dffecca68288f1d4e2
Saranki M <m.saranki@gmail.com>
2018-10-04 05:38:01 +0530

    Created the base project for double submit cookie

---
+++ src/main/java/com/sliit/ssd/doublesubmitcookie/DoubleSubmitCookieApplication.java
@@ -0,0 +1,12 @@
+package com.sliit.ssd.doublesubmitcookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleSubmitCookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleSubmitCookieApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sliit/ssd/doublesubmitcookie/DoubleSubmitCookieApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sliit.ssd.doublesubmitcookie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoubleSubmitCookieApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
